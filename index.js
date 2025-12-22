dbe5ce34bec40cb0e6633500a067d7033092db77
masx200 <masx200@qq.com>
2025-12-22 17:13:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 #!/usr/bin/env node
-require('child_process').execSync('bash start.sh', { stdio: 'inherit' });
+require('child_process').execSync('bash start.sh', {
+  stdio: "inherit",
+  env: {
+    REALITY_PORT: 20143,
+    HY2_PORT: 20143,
+  },
+});
